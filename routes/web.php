7ea6c39efd79510f8732d4969df20cfdb5f7795d
univstudentkoyo <ikoyoh0123@gmail.com>
2023-06-26 15:01:49 +0900

    Add_基礎実装_初期設定

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+use App\Http\Controllers\PostController;
+
+Route::get('/posts',[PostController::class, 'index']);
